08ee22946fe7958829f5548bd55f43eef5e2d468
Devo App <57620248+Ahmedbizo@users.noreply.github.com>
2021-12-15 01:35:09 +0300

    add some edit to car info page

--- src/components/CarInfo.tsx
+++ src/components/CarInfo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import cars from "../../public/api/cars.json";
-import { Container, Row, Card } from "react-bootstrap";
+import { Container, Row, Card, Placeholder } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {Price} from '../../pages/price';
 
@@ -16,6 +16,7 @@ interface cars {
 export const CarInfo: React.FC = () => {
   return (
     <div>
+          
     < Container fluid className="card-example d-flex  flex-nowrap ">
     {cars.map((item, i) =>
     <Row className="card-example d-flex  flex-nowrap " >  
@@ -36,6 +37,7 @@ export const CarInfo: React.FC = () => {
     
     )}  
 
+      
     
   </Container>
  
