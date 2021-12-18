07ce0bfb0ffac44bf58d413d355a9b818e8c3ded
Devo App <57620248+Ahmedbizo@users.noreply.github.com>
2021-12-19 00:07:08 +0300

    add buttom to contact the company

--- src/components/CarInfo.tsx
+++ src/components/CarInfo.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import cars from "../../public/api/cars.json";
-import { Container, Row, Card, Placeholder, Form, Button, Nav } from "react-bootstrap";
+import { Container, Row, Card, Nav, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 
@@ -21,13 +21,13 @@ export const CarInfo: React.FC = () => {
             onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
           >
             <Nav.Item>
-              <Nav.Link href="/home">Active</Nav.Link>
+              <Nav.Link href="/home">Home</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey="link-1">Link</Nav.Link>
+              <Nav.Link eventKey="Price">Price</Nav.Link>
             </Nav.Item>
             <Nav.Item>
-              <Nav.Link eventKey="link-2">Link</Nav.Link>
+              <Nav.Link eventKey="Learn">Learn</Nav.Link>
             </Nav.Item>
             <Nav.Item>
               <Nav.Link eventKey="disabled" disabled>
@@ -35,33 +35,27 @@ export const CarInfo: React.FC = () => {
               </Nav.Link>
             </Nav.Item>
           </Nav>
-          <Form>
-            <Form.Group className="md-8" controlId="formBasicEmail">
-              <Form.Label>Choose Car </Form.Label>
-              <Form.Control type="input" placeholder="Car Name" />
-            
-            </Form.Group>
-
-          </Form>
+              
     < Container fluid className="card-example d-flex  flex-nowrap ">
-    {cars.map((item, i) =>
-    <Row className="card-example d-flex  flex-nowrap " >  
-    <Card className="card-w">
-        <Card.Title className="header1" key={i}>{item.bodyType}</Card.Title>
-        <Card.Body>
-        <Card.Text className="header"> <h2 className="header-font">{item.modelName} </h2> {item.modelType}</Card.Text>
-        
-        <Card.Img className="card" style={{width:625,height:450}}  src={item.imageUrl}/>
-        <Card.Text className="price"> Prise : {item.price}</Card.Text>
-        <a href="/learn/" className="btn btnn-link" role="button">LEARN </a>
-        <a href="/shop/" className="btn btnn-link" role="button">SHOP </a>
-       
-        
-        </Card.Body>
-        </Card>
-    </Row>
-    
-    )}  
+                {cars.map((item, i) =>
+                <Row className="card-example d-flex  flex-nowrap " >  
+                <Card className="card-w">
+                    <Card.Title className="header1" key={i}>{item.bodyType}</Card.Title>
+                    <Card.Body>
+                    <Card.Text className="header"> <h2 className="header-font">{item.modelName} </h2> {item.modelType}</Card.Text>
+                    
+                    <Card.Img className="card" style={{width:625,height:450}}  src={item.imageUrl}/>
+                    <Card.Text className="price"> Prise : {item.price}</Card.Text>
+                    <a href="/learn/" className="btn btnn-link" role="button">LEARN </a>
+                    <a href="/shop/" className="btn btnn-link" role="button">SHOP </a>
+                    <Button style={{color:'yellow'}} >contact oss</Button>
+                  
+                    
+                    </Card.Body>
+                    </Card>
+                </Row>
+                
+                )}
   </Container>
  
   </div>
